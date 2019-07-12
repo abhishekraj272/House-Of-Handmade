cd4f47e87e7772b742c51258864b13e58e3037c6
Aryama <aryamasinha864@gmail.com>
2019-07-12 16:53:43 +0530

    adding changes

--- HandiCraft/views.py
+++ HandiCraft/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_page(request):
+    return render(request,"login.html")
+
+def signup_page(request):
+    return render(request,"signup.html")
+
+def sweaters_page(request):
+    return render(request,"sweaters.html")
+
+def earrings_page(request):
+    return render(request,"earrings.html")
+
+def vases_page(request):
+    return render(request,"vases.html")
+
+def scarfs_page(request):
+    return render(request,"scarfs.html")
+
+def socks_page(request):
+    return render(request,"socks.html")
--- myproject/urls.py
+++ myproject/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_page
+from HandiCraft.views import (login_page,signup_page,scarfs_page,socks_page,earrings_page,sweaters_page,vases_page)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
+    path('login/', login_page),
+    path('signup/', signup_page),
+    path('sweaters/', sweaters_page),
+    path('vases/', vases_page),
+    path('scarfs/', scarfs_page),
+    path('earrings/',earrings_page),
+    path('socks/',socks_page),
 ]
