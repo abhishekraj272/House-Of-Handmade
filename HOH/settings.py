5db0695ee5e6fef135c88bab601a899ee4149b85
aryama <aryamasinha864@gmail.com>
2020-07-06 00:50:21 +0530

    added templates

--- HOH/settings.py
+++ HOH/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a0q%tooqzv7roady$2p&4ublqqz$-9zo2t_-52rw+&m^&nb5_='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
